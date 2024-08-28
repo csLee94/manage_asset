c801db33eb100f1ba2a34668b94b199db9a3fb25
csLee94 <lcs625@naver.com>
2024-08-28 17:47:30 +0900

    add module which connect to db

--- asset-tracker-api/src/app.module.ts
+++ asset-tracker-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LibDbModule } from './libs/lib-db.module';
 
 @Module({
-  imports: [],
+  imports: [LibDbModule],
   controllers: [AppController],
   providers: [AppService],
 })
